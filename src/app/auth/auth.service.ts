792983b1ba05050b058cfb3bbd02fe5e90db8c1d
marvin4keap <marvin.abisrror@keap.com>
2023-08-23 21:35:23 -0500

    token expiration

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,16 +7,29 @@ import * as appSettingsModule from "@nativescript/core/application-settings";
 import { HttpClient } from "@angular/common/http";
 import { Config } from "~/config";
 
-const AUTHENTICATED_KEY = "AUTHENTICATED_KEY";
+const ACCESS_TOKEN_KEY = "ACCESS_TOKEN_KEY";
+const EXPIRATION_KEY = "EXPIRATION_KEY";
 
 @Injectable()
 export class AuthService {
   public get isAuthenticated(): boolean {
-    return appSettingsModule.getBoolean(AUTHENTICATED_KEY);
+    const expires = appSettingsModule.getNumber(EXPIRATION_KEY);
+    if (!expires) {
+      return false;
+    }
+    return new Date().getTime() < expires;
   }
 
-  public set isAuthenticated(value: boolean) {
-    appSettingsModule.setBoolean(AUTHENTICATED_KEY, value);
+  public getToken(): string {
+    return appSettingsModule.getString(ACCESS_TOKEN_KEY);
+  }
+
+  public setToken(token: string): void {
+    appSettingsModule.setString(ACCESS_TOKEN_KEY, token);
+  }
+
+  private setExpiration(expires: number): void {
+    appSettingsModule.setNumber(EXPIRATION_KEY, expires * 1000);
   }
 
   constructor(private http: HttpClient) {}
@@ -29,6 +42,8 @@ export class AuthService {
       .pipe(
         tap((result) => {
           console.log("token received: ", result.access_token);
+          this.setToken(result.access_token);
+          this.setExpiration(Number(result.expires));
         })
       );
   }
@@ -40,7 +55,8 @@ export class AuthService {
   }
 
   public logout(): Observable<any> {
-    this.isAuthenticated = false;
+    this.setToken(null);
+    this.setExpiration(0);
     return of(null);
   }
 }
--- src/app/item/item.service.ts
+++ src/app/item/item.service.ts
@@ -1,20 +1,28 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
 
 import { Item } from "./item";
 import { Observable } from "rxjs";
 import { map } from "rxjs/operators";
 import { Config } from "../../config";
+import { AuthService } from "../auth/auth.service";
 
 @Injectable({
   providedIn: "root",
 })
 export class ItemService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private authService: AuthService) {}
 
   getItems(): Observable<Item[]> {
+    const token = this.authService.getToken();
+    const headers = new HttpHeaders({
+      Authorization: `Bearer ${token}`,
+    });
+
     return this.http
-      .get<{ players: Item[] }>(`${Config.apiUrl}/players`)
+      .get<{ players: Item[] }>(`${Config.apiUrl}/players`, {
+        headers,
+      })
       .pipe(map((response) => response.players));
   }
 
